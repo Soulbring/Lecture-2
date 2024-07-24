bf6ed8d4970b77d402f50efc48dd9c71c85ed524
Soulbring <lobakin_vova@vk.com>
2024-07-24 15:07:39 +0400

    add task3

---
+++ LectureTask/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//int n = 10;
+int[] array = { 2, 5, 8, 13, 45, 25, 36, 20, 80, 12 };
+int i = 0;
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
