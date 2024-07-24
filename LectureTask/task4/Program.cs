145326ba4cb98d76868abc5b5c3d9dc10eb27530
Soulbring <lobakin_vova@vk.com>
2024-07-24 15:16:16 +0400

    add task4

--- LectureTask/task4/Program.cs
+++ LectureTask/task4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 5;
+int[] array = {2,4,3,9,6};
+int i = 0;
+int max = array[0];
+while( i < n)
+{
+    if(array[i]>max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.Write(max);
