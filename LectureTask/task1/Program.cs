7db0a4ba2531a123512d0009969c5ceb4da79374
Soulbring <lobakin_vova@vk.com>
2024-07-24 14:45:03 +0400

    add task1

--- LectureTask/task1/Program.cs
+++ LectureTask/task1/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] arr = new int [5];
+﻿int[] arr = new int[5];
 arr[0] = 5;
 arr[1] = 4;
 arr[2] = 6;
@@ -9,3 +9,10 @@ Console.WriteLine(arr[1]);
 Console.WriteLine(arr[2]);
 Console.WriteLine(arr[3]);
 Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 1, 3, 5, 8, 10 };
+Console.WriteLine(arr2[0]);
+Console.WriteLine(arr2[1]);
+Console.WriteLine(arr2[2]);
+Console.WriteLine(arr2[3]);
+Console.WriteLine(arr2[4]);
