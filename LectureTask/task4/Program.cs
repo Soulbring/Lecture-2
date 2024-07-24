f2cb883446d4dfb28fa37064bb206222e62a107e
Soulbring <lobakin_vova@vk.com>
2024-07-24 15:28:23 +0400

    add task4:edit code

--- LectureTask/task4/Program.cs
+++ LectureTask/task4/Program.cs
@@ -1,12 +1,23 @@
 ﻿int n = 5;
-int[] array = {2,4,3,9,6};
+int[] array = { 2, 4, 3, 9, 6 };
 int i = 0;
 int max = array[0];
-while( i < n)
+while (i < n)
 {
-    if(array[i]>max)
+    if (array[i] > max)
     {
         max = array[i];
+    }
+    i = i + 1;
+}
+Console.Write(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
     }
     i = i + 1;
 }
