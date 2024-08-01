6fd76326dbc4808992b58a23c2241d374faa6ca2
Soulbring <lobakin_vova@vk.com>
2024-08-01 10:12:46 +0400

    Add Task 4

---
+++ Homework/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int n =number;
+if(n>10)
+{
+    while (n>0)
+    {
+        int remains = n % 10;
+        System.Console.Write(remains + ", ");
+        n /=10;
+    }
+}
+else
+{
+    System.Console.WriteLine(number);
+}
