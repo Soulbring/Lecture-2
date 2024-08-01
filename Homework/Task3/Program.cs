4fcccf426daa05b6ddb835c058c7d0ce25c76e07
Soulbring <lobakin_vova@vk.com>
2024-08-01 09:43:31 +0400

    Add Task 3

---
+++ Homework/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+
+System.Console.WriteLine("Введите двузначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FirstDigit= number % 10;
+int SecondDigit= number / 10;
+
+if (number > 9 && number < 100)
+{
+    if (FirstDigit > SecondDigit)
+    {
+        System.Console.WriteLine($"Первая цифра больше и это {FirstDigit}");
+    }
+    else if (SecondDigit > FirstDigit)
+    {
+        System.Console.WriteLine($"Вторая цифра больше и это {SecondDigit}");
+    }
+    else
+    {
+        System.Console.WriteLine("Числа равны");
+    }
+    
+}
+else
+    {
+        System.Console.WriteLine("Вы ввели не двузначное число");
+    }
